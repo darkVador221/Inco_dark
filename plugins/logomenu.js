407482c1f16ae3e7a4a7071231b439ba0d627c0e
darkVador221 <vador2899@gmail.com>
2025-05-25 20:10:29 +0000

    Create logomenu.js

---
+++ plugins/logomenu.js
@@ -0,0 +1,75 @@
+const axios = require("axios");
+const { cmd } = require("../command");
+
+
+cmd({
+    pattern: "logo",
+    alias: ["logomenu"],
+    desc: "menu the bot",
+    category: "menu",
+    react: "🎀",
+    filename: __filename
+}, 
+async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply }) => {
+    try {
+        let dec = `*▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰
+╔═══✪〘 🎮 𝙇𝙊𝙂𝙊 𝘾𝙊𝙇𝙇𝙀𝘾𝙏𝙄𝙊𝙉 〙✪══⊷❍
+║🔦 ➺ .ɴᴇᴏɴʟɪɢʜᴛ
+║🖤 ➺ .ʙʟᴀᴄᴋᴘɪɴᴋ
+║🐉 ➺ .ᴅʀᴀɢᴏɴʙᴀʟʟ
+║🕶️ ➺ .𝟹ᴅᴄᴏᴍɪᴄ
+║🇺🇸 ➺ .ᴀᴍᴇʀɪᴄᴀ
+║🌀 ➺ .ɴᴀʀᴜᴛᴏ
+║😢 ➺ .sᴀᴅɢɪʀʟ
+║☁️ ➺ .ᴄʟᴏᴜᴅs
+║🚀 ➺ .ғᴜᴛᴜʀɪsᴛɪᴄ
+║📄 ➺ .𝟹ᴅᴘᴀᴘᴇʀ
+║✏️ ➺ .ᴇʀᴀsᴇʀ
+║🌅 ➺ .sᴜɴsᴇᴛ
+║🍃 ➺ .ʟᴇᴀғ
+║🌌 ➺ .ɢᴀʟᴀxʏ
+║💀 ➺ .sᴀɴs
+║💥 ➺ .ʙᴏᴏᴍ
+║👨💻 ➺ .ʜᴀᴄᴋᴇʀ
+║😈 ➺ .ᴅᴇᴠɪʟᴡɪɴɢs
+║🇳🇬 ➺ .ɴɪɢᴇʀɪᴀ
+║💡 ➺ .ʙᴜʟʙ
+║👼 ➺ .ᴀɴɢᴇʟᴡɪɴɢs
+║♌ ➺ .ᴢᴏᴅɪᴀᴄ
+║💎 ➺ .ʟᴜxᴜʀʏ
+║🎨 ➺ .ᴘᴀɪɴᴛ
+║❄️ ➺ .ғʀᴏᴢᴇɴ
+║🏰 ➺ .ᴄᴀsᴛʟᴇ
+║🖋️ ➺ .ᴛᴀᴛᴏᴏ
+║🔫 ➺ .ᴠᴀʟᴏʀᴀɴᴛ
+║🐻 ➺ .ʙᴇᴀʀ
+║🔠 ➺ .ᴛʏᴘᴏɢʀᴀᴘʜʏ
+║🎂 ➺ .ʙɪʀᴛʜᴅᴀʏ
+╚══════════════════⊷❍
+▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰*`;
+
+        await conn.sendMessage(
+            from,
+            {
+                image: { url: `https://files.catbox.moe/zzne7x.jpeg` },
+                caption: dec,
+                contextInfo: {
+                    mentionedJid: [m.sender],
+                    forwardingScore: 999,
+                    isForwarded: true,
+                    forwardedNewsletterMessageInfo: {
+                        newsletterJid: '120363382023564830@newsletter',
+                        newsletterName: "GAMER-XMD LOGO MENU 🎮 ₊",
+                        serverMessageId: 143
+                    }
+                }
+            },
+            { quoted: mek }
+        );
+
+    } catch (e) {
+        console.log(e);
+        reply(`${e}`);
+    }
+});
+          
